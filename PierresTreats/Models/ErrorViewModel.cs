48db7c0e896f7894a670798bdac82361eb9e21a8
Justin Shaffer <justinbshaffer91@gmail.com>
2023-06-15 12:44:08 -0700

    Initial commit: Set up project structure and installed necessary dependencies

---
+++ PierresTreats/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PierresTreats.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
